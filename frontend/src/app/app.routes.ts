8bcb6cda837b14299854b0b38a8029190e58653b
Kristjan Cuznar <kristjan.cuznar@sij-s.si>
2026-03-10 14:00:18 +0100

    Add guard for dashboard.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login/login.page';
 import { DashboardPageComponent } from './pages/dashboard/dashboard.page';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
-  { path: 'dashboard', component: DashboardPageComponent },
+  { path: 'dashboard', component: DashboardPageComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: 'dashboard' },
 ];
